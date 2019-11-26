cd7aead303904d89ca6cec1c37a12c2370a95c07
ch-ivy <chivy1221@gmail.com>
2019-11-26 10:35:03 +0200

    Fourth commit

--- src/app/dept-detail/dept-detail.component.ts
+++ src/app/dept-detail/dept-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, ParamMap } from '@angular/router';
+
 
 @Component({
   selector: 'app-dept-detail',
@@ -7,18 +8,39 @@ import { ActivatedRoute } from '@angular/router';
     <h3>
       You have entered {{departmentId}}'s page
     </h3>
-    <button class = "badge" routerLink = "/departments"> Back </button>
+    <a (click)='goPrevious()'  class = "card"> Previous </a>
+    <a (click)='goNext()' class = "card"> Next </a>
+    <button (click)='goToDept()' class = "badge"> Back to main list </button>
   `,
   styles: []
 })
 export class DeptDetailComponent implements OnInit {
 
   public departmentId;
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
-    let id = parseInt(this.route.snapshot.paramMap.get('id'));
-    this.departmentId = id;
-  }
+   // let id = parseInt(this.route.snapshot.paramMap.get('id'));
+    //this.departmentId = id;
 
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.departmentId = id;
+    });
+  }
+  goPrevious()
+  {
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments',previousId]);
+  }
+  goNext()
+  {
+    let nextId = this.departmentId + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
+  goToDept()
+  {
+    let selectedId = this.departmentId ? this.departmentId : null;
+    this.router.navigate(['/departments', {id: selectedId}]);
+  }
 }
--- src/app/dept/dept.component.ts
+++ src/app/dept/dept.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-dept',
   template: `
   <h2> Department List </h2>
   <ul class="items">
-    <li (click)="onSelect(info)" *ngFor="let info of departments">
+    <li (click)="onSelect(info)"  [class.selected]="isSelected(info)" *ngFor="let info of departments">
         <span class = "badge"> {{info.id}} </span> {{info.name}} 
     </li>
   </ul>
@@ -23,14 +23,22 @@ export class DeptComponent implements OnInit {
     {"id" : 4, "name" : "Ruby"},
     {"id" : 5, "name" : "Bootstrap"},
   ];
-
-  constructor(private router: Router) { }
+  public selectedId;
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let id = parseInt(params.get('id'));
+      this.selectedId = id;
+    })
   }
 
   onSelect(info)
   {
       this.router.navigate(['/departments', info.id])
+  }
+  isSelected(info)
+  {
+    return info.id === this.selectedId;
   }
 }
